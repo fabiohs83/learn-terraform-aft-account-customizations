8e95f788bd3baa8fd4c78b084d3afe682ae7d39b
code-commit-at-121943979533 <fahbio@gmail.com>
2022-07-01 12:55:59 -0300

    customization-s3-tag edited

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
   tags = {
-    Name        = "My AFT bucket"
-    # Environment = "Dev"
+    Name        = "My AFT bucket-${data.aws_caller_identity.current.account_id}"
+    Pipeline    = "aft-account-customizations"
   }
 }
